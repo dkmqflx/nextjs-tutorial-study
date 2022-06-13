c09df098ff4abc62c2867b2f224cfec536502215
dkmqflx <dkmqflx@gmail.com>
2022-06-13 23:38:35 +0900

    10. Link Component Navigation

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,11 +1,41 @@
-function Product() {
+import Link from 'next/link';
+
+function Product({ productId = 100 }) {
   return (
     <>
-      <h1>Product 1</h1>
-      <h1>Product 2</h1>
-      <h1>Product 3</h1>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+
+      <h2>
+        <Link href="/product/1">
+          <a>Product 1</a>
+        </Link>
+      </h2>
+      <h2>
+        <Link href="/product/2">
+          <a>Product 2</a>
+        </Link>
+      </h2>
+      <h2>
+        <Link href="/product/3" replace>
+          <a>Product 3</a>
+        </Link>
+      </h2>
+
+      <h2>
+        <Link href={`/product/${productId}`}>
+          <a>Product {productId}</a>
+        </Link>
+      </h2>
     </>
   );
 }
 
 export default Product;
+
+/**
+ *
+ * page so the replayce prop will replace the current history state
+ * instead of adding a new url to the stack
+ */
