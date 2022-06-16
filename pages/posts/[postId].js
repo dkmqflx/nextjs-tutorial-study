f81dd2d5a9c077ed552ba4bfa8af2d3e42d05446
dkmqflx <dkmqflx@gmail.com>
2022-06-16 15:21:59 +0900

    23. SSG with getStaticPaths

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -11,6 +11,23 @@ function Post({ post }) {
 
 export default Post;
 
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: { postId: '1' },
+      },
+      {
+        params: { postId: '2' },
+      },
+      {
+        params: { postId: '3' },
+      },
+    ],
+    fallback: false,
+  };
+}
+
 export async function getStaticProps(context) {
   const { params } = context;
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`);
@@ -21,3 +38,18 @@ export async function getStaticProps(context) {
     },
   };
 }
+
+/*
+
+- we need to return an object
+- this object must contain a paths key
+- this key determines which paths will be statically generated at build time
+- the key is an array of objects
+- each object contains a params key which in turn is an object that contains the route parameter with its value
+- so object the route parameter is post id we need to specify a value
+
+- there is one last thing the returned object must contain another key called fallback
+- we will talk more about what role this key plays in the near future but for now set its value to false
+
+- if i view page source we see the data here as well we have successfully pre-rendered a page with dynamic parameters
+*/
