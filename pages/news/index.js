b6f811dc163e146f04d0300c242ffc184fcfebe2
dkmqflx <dkmqflx@gmail.com>
2022-06-18 23:16:22 +0900

    35.Inspecting SSR Builds.md

--- pages/news/index.js
+++ pages/news/index.js
@@ -21,6 +21,7 @@ export default NewsArticleList;
 export async function getServerSideProps() {
   const response = await fetch('http://localhost:4000/news');
   const data = await response.json();
+  console.log('Pre-rendering NewsArticleList');
 
   return {
     props: {
