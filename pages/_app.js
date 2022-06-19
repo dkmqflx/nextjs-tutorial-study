97ccb681c347ba12b254d421f4f3d0f51b8aeec9
dkmqflx <dkmqflx@gmail.com>
2022-06-19 22:13:53 +0900

    50. Global Styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+// global style이 이 파일에 import 해서 적용되어 있다.
+// 그래서 모든 페이지에 적용된다
+import 'bootstrap/dist/css/bootstrap.min.css';
+// using bootstrap class
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <div>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-success">Success</button>
+      <h2>About Page</h2>;
+    </div>
+  );
+}
+
+export default About;
