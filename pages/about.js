8e07df7e19e526e5bcf4ee1c0dd0ba6a24352434
dkmqflx <dkmqflx@gmail.com>
2022-06-19 23:01:53 +0900

    52. SASS Support

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,13 @@
-import styles from '../styles/About.module.css';
+// import styles from '../styles/About.module.css';
+import styles from '../styles/About.module.scss';
 
 function About() {
   return (
     <div>
       <button className="btn btn-primary">Primary</button>
       <button className="btn btn-success">Success</button>
-      <h2 className={styles.highlight}>About Page</h2>;
+      {/* <h2 className={styles.highlight}>About Page</h2>; */}
+      <h2 className={styles.highlightscss}>About Page</h2>;
     </div>
   );
 }
--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,9 @@
-import styles from '../styles/Contact.module.css';
+// import styles from '../styles/Contact.module.css';
+import styles from '../styles/Contact.module.scss';
 
 function Contact() {
-  return <h2 className={styles.highlight}>Contact Page</h2>;
+  // return <h2 className={styles.highlight}>Contact Page</h2>;
+  return <h2 className={styles.highlightscss}>Contact Page</h2>;
 }
 
 export default Contact;
