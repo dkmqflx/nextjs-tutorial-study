d8776d8249ea66fae8d0941c9ce0fe679d1c1168
dkmqflx <dkmqflx@gmail.com>
2022-06-19 22:54:39 +0900

    51. Component Level Styles

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,11 @@
+import styles from '../styles/About.module.css';
+
 function About() {
   return (
     <div>
       <button className="btn btn-primary">Primary</button>
       <button className="btn btn-success">Success</button>
-      <h2>About Page</h2>;
+      <h2 className={styles.highlight}>About Page</h2>;
     </div>
   );
 }
--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,7 @@
+import styles from '../styles/Contact.module.css';
+
 function Contact() {
-  return <h2>Contact Page</h2>;
+  return <h2 className={styles.highlight}>Contact Page</h2>;
 }
 
 export default Contact;
