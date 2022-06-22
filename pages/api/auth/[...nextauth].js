d65ff1c7e1c7e6d176121e79a8a84148d0ff775e
dkmqflx <dkmqflx@gmail.com>
2022-06-22 23:03:37 +0900

    77. Callbacks

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,13 +9,18 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.id = token.id;
+      return session;
+    },
+  },
 });
 
-// /api/auth/signin
-// next js에서 제공하는 login 페이지
-// login 하면 Cookies에서 값이 생긴 것을 확인할 수 있다.
-// next-auth가 jwt를 사용하기 때문이다
-
-// /api/auth/signout
-// log out 페이지
-// log out 버튼 누르면 쿠키 삭제되는 것을 확인할 수 있다.
+// callback 함수가 실행된다
